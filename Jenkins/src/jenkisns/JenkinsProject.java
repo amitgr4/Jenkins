85b088e06017827ee6075a8cd61ac622b3da43b6
amitgr4 <amitgr4@gmail.com>
2024-01-05 00:55:12 +0530

    "commit jenkins"

---
+++ Jenkins/src/jenkisns/JenkinsProject.java
@@ -0,0 +1,13 @@
+package jenkisns;
+
+public class JenkinsProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is java project learn git");
+		System.out.println("this is jenkins");
+	    
+
+	}
+
+}
